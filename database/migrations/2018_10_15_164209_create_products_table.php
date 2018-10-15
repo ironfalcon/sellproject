d4ea5a7d7fc4e1b8329d5e01a93ded9a0269dddc
ironfalcon <ironfalcon@yandex.ru>
2018-10-15 21:59:56 +0400

    Adding model, resource, migration for (Product and Photo)

---
+++ database/migrations/2018_10_15_164209_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('description');
+            $table->float('price', 8, 2);
+            $table->integer('count');
+            $table->string('type', 50);
+            $table->string('meta-keywords', 100);
+            $table->string('meta-description', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
